69029805f1b61dd299e225664e2ef54785832859
Konrad War <marek.dudek@protonmail.ch>
2018-12-06 21:04:54 +0100

    i collect currency form NBP.

--- program.py
+++ program.py
@@ -12,7 +12,6 @@ from bs4 import BeautifulSoup
 
 now = datetime.datetime.now()
 data = ("%s-%s-%s" % (now.year, now.month, now.day))
-
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # i added data to file JSON
 # data = {'server' : 'localhost',
@@ -42,27 +41,25 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 def currency():
     dict = {}
-    if soup.find_all('td')[35].get_text() == '1 EUR':
-        dict[soup.find_all('td')[35].get_text()] = (soup.find_all('td')[36].get_text()).replace(",", ".")
-    if soup.find_all('td')[37].get_text() == '1 USD':
-        dict[soup.find_all('td')[37].get_text()] = (soup.find_all('td')[38].get_text()).replace(",", ".")
-    if soup.find_all('td')[39].get_text() == '1 CHF':
-        dict[soup.find_all('td')[39].get_text()] = (soup.find_all('td')[40].get_text()).replace(",", ".")
-    if soup.find_all('td')[41].get_text() == '1 GBP':
-        dict[soup.find_all('td')[41].get_text()] = (soup.find_all('td')[42].get_text()).replace(",", ".")
+    if soup.find_all('td')[36].get_text() == '1 EUR':
+        dict[soup.find_all('td')[37].get_text()] = (soup.find_all('td')[37].get_text()).replace(",", ".")
+    if soup.find_all('td')[38].get_text() == '1 USD':
+        dict[soup.find_all('td')[39].get_text()] = (soup.find_all('td')[39].get_text()).replace(",", ".")
+    if soup.find_all('td')[40].get_text() == '1 CHF':
+        dict[soup.find_all('td')[41].get_text()] = (soup.find_all('td')[41].get_text()).replace(",", ".")
+    if soup.find_all('td')[42].get_text() == '1 GBP':
+        dict[soup.find_all('td')[43].get_text()] = (soup.find_all('td')[43].get_text()).replace(",", ".")
     return dict
 
 # logging.info(currency())
-
-# key - currency
-# v - value
 for k, v in currency().items():
-    logging.debug(k, ast.literal_eval(v))
+    # logging.debug(k, ast.literal_eval(v))
     question = ("BEGIN TRAN "
                 "IF NOT EXISTS "
                 "(SELECT id FROM dbo.currency with (updlock, rowlock, holdlock) where currency= '%s' and date='%s') "
                 "INSERT INTO dbo.currency(currency, value, date) VALUES ('%s',%f,'%s') "
-                "COMMIT") % (k, data, k, ast.literal_eval(v), data)
+                "COMMIT"
+                ) % (k, data, k, ast.literal_eval(v), data)
 
     cursor.execute(question)
     cursor.commit()
@@ -73,7 +70,7 @@ for k, v in currency().items():
 # delete from dbo.currency
 # select * from dbo.currency
 
-# cursor.execute("SELECT * FROM dbo.currency ")
+# cursor.execute("SELECT * FROM dbo.currency")
 # for row in cursor:
 #     print('%r' % (row))
 
